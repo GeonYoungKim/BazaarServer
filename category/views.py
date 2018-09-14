a06373ac7a230d2e85f83ce23c61f8eb93794ef1
geonyoungkim <c2619zz@naver.com>
2018-09-14 11:47:44 +0900

    로깅 추가

--- category/views.py
+++ category/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+import logging
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import pymongo
@@ -12,6 +14,8 @@ FANCY = "3"
 BOOK = "4"
 ACC = "5"
 DIGITAL = "6"
+
+logger = logging.getLogger("category")
 
 def get_category(category_num,list_num):
     shops = list(collection
@@ -26,37 +30,39 @@ def get_category(category_num,list_num):
             ]
         )
     )
+    logger.info("mongod db")
     for shop in shops:
         shop["good"] = shop["goods"]
         shop.pop("goods")
+    logger.info("change shops")
     return Response(shops)
 
 @api_view(['GET'])
 def get_cloth(request, list_num):
-    print("cloth category")
+    logger.info("cloth category")
     return get_category(CLOTH,list_num)
 
 @api_view(['GET'])
 def get_digital(request, list_num):
-    print("digital category")
+    logger.info("digital category")
     return get_category(DIGITAL, list_num)
 
 @api_view(['GET'])
 def get_acc(request, list_num):
-    print("acc category")
+    logger.info("acc category")
     return get_category(ACC, list_num)
 
 @api_view(['GET'])
 def get_etc(request, list_num):
-    print("etc category")
+    logger.info("etc category")
     return get_category(ETC, list_num)
 
 @api_view(['GET'])
 def get_book(request, list_num):
-    print("book category")
+    logger.info("book category")
     return get_category(BOOK, list_num)
 
 @api_view(['GET'])
 def get_fancy(request, list_num):
-    print("fancy category")
+    logger.info("fancy category")
     return get_category(FANCY, list_num)
