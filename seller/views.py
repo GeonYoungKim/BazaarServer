c4e7188f9b3d4e674e3f35e21e3c8aedb30e45da
geonyoungkim <c2619zz@naver.com>
2018-09-27 01:56:42 +0900

    상품 등록 이미지 경로 지정

--- seller/views.py
+++ seller/views.py
@@ -40,11 +40,13 @@ def insert_goods(request):
         json_body['category'] = category_list
         location = json_body['location']
         logger.info("request body -> "+str(json_body))
-        destination = open('static\\bazaar_img\\'+str(location)+"\\" + filename_now + image_uploaded.name, "wb+")
+        image_path = 'static\\bazaar_img\\'+str(location)+"\\" + filename_now + image_uploaded.name
+        destination = open(image_path, "wb+")
         for chunk in image_uploaded.chunks():
             destination.write(chunk)
         destination.close()
-        json_body['image']=filename_now + image_uploaded.name
+        image_path = image_path.replace("\\","/")
+        json_body['image']="http://13.125.128.130/"+image_path
         shop = json_body['shop']
         json_body.pop('shop')
         json_body.pop('location')
