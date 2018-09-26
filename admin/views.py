82a588b8edb2d33980306d74c34dbfd14e07acbe
geonyoungkim <c2619zz@naver.com>
2018-09-26 22:54:14 +0900

    admin 승인 판매자 메시지 전송 구현 1. shop.goods 초기화 2. 승인된 판매자들 location,shop,role 지정 3. 클라이언트 파이어베이스 붙일 시 서버 연동 시작

--- admin/views.py
+++ admin/views.py
@@ -8,9 +8,15 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from BazaarServer.settings import apply_collection
+from BazaarServer.settings import apply_collection, shop_collection, user_collection
 
+SUCCESS = {"response": "success"}
+FAIL = {"response": "fail"}
 logger = logging.getLogger("admin")
+
+location = ["a", "b", "c", "d", "e"]
+
+
 def select_all_apply(list_num):
     data = {}
     applies = list(apply_collection.aggregate(
@@ -21,7 +27,7 @@ def select_all_apply(list_num):
                 "id": 1,
                 "title": 1,
                 "contents": 1,
-                "role":1,
+                "role": 1,
                 "name": 1,
                 "date": 1
             }}
@@ -31,18 +37,19 @@ def select_all_apply(list_num):
         ]
     ))
     data['items'] = applies
-    data['meta'] = {'count':len(applies)}
+    data['meta'] = {'count': len(applies)}
     return data
+
 
 def give_apply_role(applies):
     for apply in applies:
         apply_collection.update(
             {
-                "id":apply['id']
+                "id": apply['id']
             },
             {
-                "$set":{
-                    "role":2
+                "$set": {
+                    "role": 2
                 }
             }
         )
@@ -57,16 +64,18 @@ def give_apply_role(applies):
         }
     )
 
-@api_view(['GET'])
-def get_apply(request,list_num):
-    logger.info("get_apply")
-    logger.info("list_num_ ->"+str(list_num))
-    return Response(select_all_apply(list_num))
 
 @api_view(['GET'])
-def random_assignment(request,count,list_num):
+def get_apply(request, list_num):
+    logger.info("get_apply")
+    logger.info("list_num_ ->" + str(list_num))
+    return Response(select_all_apply(list_num))
+
+
+@api_view(['GET'])
+def random_assignment(request, count, list_num):
     logger.info("random_assignment")
-    logger.info("count -> "+str(count))
+    logger.info("count -> " + str(count))
     logger.info("list_num -> " + str(list_num))
     applies = list(apply_collection.aggregate(
         [
@@ -80,7 +89,7 @@ def random_assignment(request,count,list_num):
                 "name": 1,
                 "date": 1
             }}
-            ,{"$sample":{"size":count}}
+            , {"$sample": {"size": count}}
         ]
     ))
     logger.info("after random extraction")
@@ -88,8 +97,9 @@ def random_assignment(request,count,list_num):
     logger.info("after give_apply_role ")
     return Response(select_all_apply(list_num))
 
+
 @api_view(['GET'])
-def firstcome_assignment(request,count,list_num):
+def firstcome_assignment(request, count, list_num):
     logger.info("firstcome_assignment")
     logger.info("count -> " + str(count))
     logger.info("list_num -> " + str(list_num))
@@ -112,10 +122,11 @@ def firstcome_assignment(request,count,list_num):
     logger.info("after firstcome extraction")
     give_apply_role(applies)
     logger.info("after give_apply_role")
-    return  Response(select_all_apply(list_num))
+    return Response(select_all_apply(list_num))
+
 
 @api_view(['POST'])
-def one_assignment(request,list_num):
+def one_assignment(request, list_num):
     logger.info("one_assignment")
     logger.info("list_num -> " + str(list_num))
     json_body = json.loads(request.body.decode("utf-8"))
@@ -132,7 +143,45 @@ def one_assignment(request,list_num):
     logger.info("apply_role_update")
     return Response(select_all_apply(list_num))
 
-@api_view(['POST'])
+
+@api_view(['GET'])
 def send_admission(request):
-#   파이어베이스 연동
-    pass
+    # shop goods 초기화
+    shop_collection.update_many(
+        {},
+        {"$set": {"goods": []}}
+    )
+    applies = list(apply_collection.aggregate(
+        [
+            {"$match": {"role": 2}},
+            {"$project": {
+                "_id": 0,
+                "id": 1,
+                "title": 1,
+                "contents": 1,
+                "role": 1,
+                "name": 1,
+                "date": 1
+            }}
+        ]
+    ))
+    location_index = 0
+    shop_index = 1
+
+    for apply in applies:
+        if shop_index == 81:
+            shop_index = 1
+            location_index += 1
+        user_collection.update(
+            {"id": apply['id']},
+            {"$set": {
+                "role": apply['role'],
+                "shop": location[location_index].upper()+str(shop_index),
+                "location": location[location_index]
+            }
+        }
+        )
+        shop_index += 1
+
+    #   파이어베이스 연동
+    return Response(SUCCESS)
